8aa4a8456b1a99d801860b4d282c8c2811aa02a7
Marius Meißner <info@marius-meissner.net>
2022-04-05 13:36:44 +0200

    SW-101: Added methods for changing output state

--- src/expander.rs
+++ src/expander.rs
@@ -54,6 +54,9 @@ where
     pub(crate) configuration_1: u8,
 }
 
+const COMMAND_OUTPUT_0: u8 = 0x02;
+const COMMAND_OUTPUT_1: u8 = 0x03;
+
 const COMMAND_CONF_0: u8 = 0x06;
 const COMMAND_CONF_1: u8 = 0x07;
 
@@ -105,6 +108,52 @@ where
             Bank::Bank0 => self.write_conf0(0xff),
             Bank::Bank1 => self.write_conf1(0xff),
         }
+    }
+
+    /// Sets the given pin to HIGH
+    /// The given pins needs to be in output note, otherwise the change has not electrical effect
+    pub fn set_high(&mut self, bank: Bank, id: PinID) -> Result<(), <B as Write>::Error> {
+        match bank {
+            Bank::Bank0 => self.write_output0(self.output_0 | 1 << id as u8),
+            Bank::Bank1 => self.write_output1(self.output_1 | 1 << id as u8),
+        }
+    }
+
+    /// Sets the given pin to LOW
+    /// The given pins needs to be in output note, otherwise the change has not electrical effect
+    pub fn set_low(&mut self, bank: Bank, id: PinID) -> Result<(), <B as Write>::Error> {
+        match bank {
+            Bank::Bank0 => self.write_output0(self.output_0 & !(1 << id as u8)),
+            Bank::Bank1 => self.write_output1(self.output_1 & !(1 << id as u8)),
+        }
+    }
+
+    /// Sets all pins of the given bank to high state
+    pub fn set_all_high(&mut self, bank: Bank) -> Result<(), <B as Write>::Error> {
+        match bank {
+            Bank::Bank0 => self.write_output0(0xff),
+            Bank::Bank1 => self.write_output1(0xff),
+        }
+    }
+
+    /// Sets all pins of the given bank to high state
+    pub fn set_all_low(&mut self, bank: Bank) -> Result<(), <B as Write>::Error> {
+        match bank {
+            Bank::Bank0 => self.write_output0(0x0),
+            Bank::Bank1 => self.write_output1(0x0),
+        }
+    }
+
+    /// Writes the first output state byte
+    fn write_output0(&mut self, conf: u8) -> Result<(), <B as Write>::Error> {
+        self.output_0 = conf;
+        self.bus.write(COMMAND_OUTPUT_0, &[self.output_0])
+    }
+
+    /// Writes the second output state byte
+    fn write_output1(&mut self, conf: u8) -> Result<(), <B as Write>::Error> {
+        self.output_1 = conf;
+        self.bus.write(COMMAND_OUTPUT_1, &[self.output_1])
     }
 
     /// Writes the first configuration byte
--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,5 @@
 use crate::expander::Bank::{Bank0, Bank1};
-use crate::expander::PinID::{Pin3, Pin4, Pin5, Pin6};
+use crate::expander::PinID::{Pin0, Pin1, Pin2, Pin3, Pin4, Pin5, Pin6, Pin7};
 use crate::expander::PCA9539;
 use crate::mocks::MockI2CBus;
 
@@ -183,4 +183,188 @@ fn test_expander_all_output_bank1() {
     expander.all_output(Bank1).unwrap();
     assert_eq!(0b1111_1111, expander.configuration_0);
     assert_eq!(0b0000_0000, expander.configuration_1);
+}
+
+#[test]
+fn test_expander_set_all_low_bank0() {
+    let mut i2c_bus = MockI2CBus::new();
+
+    i2c_bus.expect_write().times(1).returning(move |address, data| {
+        assert_eq!(0x02, address);
+
+        assert_eq!(1, data.len());
+        assert_eq!(0b0000_0000, data[0]);
+        Ok(())
+    });
+
+    let mut expander = PCA9539::new(i2c_bus);
+
+    expander.set_all_low(Bank0).unwrap();
+    assert_eq!(0b0000_0000, expander.output_0);
+    assert_eq!(0b1111_1111, expander.output_1);
+}
+
+#[test]
+fn test_expander_set_all_low_bank1() {
+    let mut i2c_bus = MockI2CBus::new();
+
+    i2c_bus.expect_write().times(1).returning(move |address, data| {
+        assert_eq!(0x03, address);
+
+        assert_eq!(1, data.len());
+        assert_eq!(0b0000_0000, data[0]);
+        Ok(())
+    });
+
+    let mut expander = PCA9539::new(i2c_bus);
+
+    expander.set_all_low(Bank1).unwrap();
+    assert_eq!(0b1111_1111, expander.output_0);
+    assert_eq!(0b0000_0000, expander.output_1);
+}
+
+#[test]
+fn test_expander_set_all_high_bank0() {
+    let mut i2c_bus = MockI2CBus::new();
+
+    i2c_bus.expect_write().times(2).returning(move |_, _| Ok(()));
+    i2c_bus.expect_write().times(1).returning(move |address, data| {
+        assert_eq!(0x02, address);
+
+        assert_eq!(1, data.len());
+        assert_eq!(0b1111_1111, data[0]);
+        Ok(())
+    });
+
+    let mut expander = PCA9539::new(i2c_bus);
+
+    expander.set_all_low(Bank0).unwrap();
+    expander.set_all_low(Bank1).unwrap();
+    expander.set_all_high(Bank0).unwrap();
+    assert_eq!(0b1111_1111, expander.output_0);
+    assert_eq!(0b0000_0000, expander.output_1);
+}
+
+#[test]
+fn test_expander_set_all_high_bank1() {
+    let mut i2c_bus = MockI2CBus::new();
+
+    i2c_bus.expect_write().times(2).returning(move |_, _| Ok(()));
+    i2c_bus.expect_write().times(1).returning(move |address, data| {
+        assert_eq!(0x03, address);
+
+        assert_eq!(1, data.len());
+        assert_eq!(0b1111_1111, data[0]);
+        Ok(())
+    });
+
+    let mut expander = PCA9539::new(i2c_bus);
+
+    expander.set_all_low(Bank0).unwrap();
+    expander.set_all_low(Bank1).unwrap();
+    expander.set_all_high(Bank1).unwrap();
+    assert_eq!(0b0000_0000, expander.output_0);
+    assert_eq!(0b1111_1111, expander.output_1);
+}
+
+#[test]
+fn test_expander_set_high_bank0() {
+    let mut i2c_bus = MockI2CBus::new();
+
+    i2c_bus.expect_write().times(2).returning(move |_, _| Ok(()));
+    i2c_bus.expect_write().times(2).returning(move |address, data| {
+        assert_eq!(0x02, address);
+
+        assert_eq!(1, data.len());
+        assert_eq!(0b0000_0010, data[0]);
+        Ok(())
+    });
+
+    let mut expander = PCA9539::new(i2c_bus);
+    expander.set_all_low(Bank0).unwrap();
+    expander.set_all_low(Bank1).unwrap();
+
+    expander.set_high(Bank0, Pin1).unwrap();
+    assert_eq!(0b0000_0010, expander.output_0);
+    assert_eq!(0b0000_0000, expander.output_1);
+
+    // Calling again does not change the result
+    expander.set_high(Bank0, Pin1).unwrap();
+    assert_eq!(0b0000_0010, expander.output_0);
+    assert_eq!(0b0000_0000, expander.output_1);
+}
+
+#[test]
+fn test_expander_set_high_bank1() {
+    let mut i2c_bus = MockI2CBus::new();
+
+    i2c_bus.expect_write().times(2).returning(move |_, _| Ok(()));
+    i2c_bus.expect_write().times(2).returning(move |address, data| {
+        assert_eq!(0x03, address);
+
+        assert_eq!(1, data.len());
+        assert_eq!(0b0000_0001, data[0]);
+        Ok(())
+    });
+
+    let mut expander = PCA9539::new(i2c_bus);
+    expander.set_all_low(Bank0).unwrap();
+    expander.set_all_low(Bank1).unwrap();
+
+    expander.set_high(Bank1, Pin0).unwrap();
+    assert_eq!(0b0000_0000, expander.output_0);
+    assert_eq!(0b0000_0001, expander.output_1);
+
+    // Calling again does not change the result
+    expander.set_high(Bank1, Pin0).unwrap();
+    assert_eq!(0b0000_0000, expander.output_0);
+    assert_eq!(0b0000_0001, expander.output_1);
+}
+
+#[test]
+fn test_expander_set_low_bank0() {
+    let mut i2c_bus = MockI2CBus::new();
+
+    i2c_bus.expect_write().times(2).returning(move |address, data| {
+        assert_eq!(0x02, address);
+
+        assert_eq!(1, data.len());
+        assert_eq!(0b1111_1011, data[0]);
+        Ok(())
+    });
+
+    let mut expander = PCA9539::new(i2c_bus);
+
+    expander.set_low(Bank0, Pin2).unwrap();
+    assert_eq!(0b1111_1011, expander.output_0);
+    assert_eq!(0b1111_1111, expander.output_1);
+
+    // Calling again does not change the result
+    expander.set_low(Bank0, Pin2).unwrap();
+    assert_eq!(0b1111_1011, expander.output_0);
+    assert_eq!(0b1111_1111, expander.output_1);
+}
+
+#[test]
+fn test_expander_set_low_bank1() {
+    let mut i2c_bus = MockI2CBus::new();
+
+    i2c_bus.expect_write().times(2).returning(move |address, data| {
+        assert_eq!(0x03, address);
+
+        assert_eq!(1, data.len());
+        assert_eq!(0b0111_1111, data[0]);
+        Ok(())
+    });
+
+    let mut expander = PCA9539::new(i2c_bus);
+
+    expander.set_low(Bank1, Pin7).unwrap();
+    assert_eq!(0b1111_1111, expander.output_0);
+    assert_eq!(0b0111_1111, expander.output_1);
+
+    // Calling again does not change the result
+    expander.set_low(Bank1, Pin7).unwrap();
+    assert_eq!(0b1111_1111, expander.output_0);
+    assert_eq!(0b0111_1111, expander.output_1);
 }
